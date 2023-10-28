5f070acbc049a5a4969a2d988a554670e7cbafe8
forvev <zel1916@o2.pl>
2023-10-28 14:25:15 +0200

    init concept

--- scripts/waste_recogintion.py
+++ scripts/waste_recogintion.py
@@ -1,8 +1,33 @@
 from fastapi import FastAPI
+import ultralytics
+from pydantic import BaseModel
+import base64
+from io import BytesIO
+from PIL import Image
 
 app = FastAPI()
 
+class PhotoData(BaseModel):
+    photo_base64: str
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/recognition")
+async def yolo_recognition(photo_data: PhotoData):
+    # Decode the base64-encoded photo data
+    try:
+        image_bytes = base64.b64decode(photo_data.photo_base64)
+        image = Image.open(BytesIO(image_bytes))
+        
+        # Here, you can process the 'image' using your recognition logic (e.g., YOLO)
+        # Example: yolo_result = perform_yolo_recognition(image)
+        
+        # For now, let's return a placeholder response
+        message = "YOLO recognition performed on the uploaded photo."
+        
+        return {"message": message}
+    except Exception as e:
+        return {"error": f"Error processing the photo: {str(e)}"}
